ed083effaf178004280bc03d1bfe09d859acd0fc
asm-saim <saim777m@gmail.com>
2026-09-18 12:08:30 +0600

    data loading using useEffect

--- app/components/SearchFood.tsx
+++ app/components/SearchFood.tsx
@@ -1,12 +1,21 @@
-import { use } from "react";
+import { use, useEffect, useState } from "react";
 import { FoodData } from "../types";
-
 interface SearchFoodPromise {
   foodPromise: Promise<FoodData[]>;
 }
 
 const SearchFood = ({ foodPromise }: SearchFoodPromise) => {
   const foodData = use(foodPromise);
+
+  //Fetching data using useEffect:
+  //   const [foodMenu, setFoodMenu] = useState();
+  // useEffect(()=>{
+  //     fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods/top-foods')
+  //     .then(res=>res.json()
+  //     .then(data=>setFoodMenu(data.data))
+  // )
+  // },[])
+
   return (
     <div>
       <h1>{foodData.length}</h1>
