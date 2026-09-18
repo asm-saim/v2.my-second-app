437dee9bbdea5570121c8d55ec55e170a5541c0d
asm-saim <saim777m@gmail.com>
2026-09-18 12:17:21 +0600

    loading file implemented

--- app/components/SearchFood.tsx
+++ app/components/SearchFood.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { use } from "react";
 import { FoodData } from "../types";
 interface SearchFoodPromise {
   foodPromise: Promise<FoodData[]>;
---
+++ app/menu/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center text-4xl">
+      <span className="loading loading-infinity loading-xl"></span>
+    </div>
+  );
+};
+
+export default loading;
